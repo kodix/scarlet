721e3213a46d5cfb8d2466ecd8e4cf8b2e99fbbe
Victor Truhan <vt@kodix.ru>
2019-08-23 15:31:40 +0300

    - client package with webpack/ts/react;

---
+++ packages/client/src/components/App.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App: React.FC = () => <div>Hello world111</div>;
+
+export default App;
---
+++ packages/client/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index',
+  output: {
+    path: path.join(__dirname, 'packages/client/lib'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
